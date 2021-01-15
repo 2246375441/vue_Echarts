cbe2a991cbbcba390df5eb5106ad86f21d58fa33
陈先生 <2246375441@qq.com>
2021-01-15 15:03:04 +0800

    😋销量趋势图表数据渲染完成

--- src/views/Trend/Trend.vue
+++ src/views/Trend/Trend.vue
@@ -35,18 +35,54 @@ export default {
       // 初始化实例
       this.chartInstance = this.$echarts.init(this.$refs.trend_ref)
       // 初始化图表设置配置项
-      const initOption = {}
+      const initOption = {
+        xAxis: {
+          type: 'category'
+        },
+        yAxis: {
+          type: 'value'
+        }
+      }
       // 渲染图表
       this.chartInstance.setOption(initOption)
     },
     // 请求数据
     async getData () {
+      // 发送请求 通过数据结构 获取内部data数据变量
+      const { data: res } = await this.$axios.get('trend')
+      // 将数据存储到data中
+      this.allData = res
       this.updateChart()
     },
     // 图表更新
     updateChart () {
+      // x轴月份数据 ['一月','二月'.....]
+      const timeArr = this.allData.common.month
+      // y轴地区数据 [{type: "line", data: [12,44,55,...], stack: "map"},{}....]
+      const valueArr = this.allData.map.data
+      // 处理y轴数据
+      const seriesArr = valueArr.map(item => {
+        return {
+          name: item.name,
+          type: 'line',
+          data: item.data,
+          stack: 'map' // 将样式转换为堆叠图
+        }
+      })
+      // 图例筛选数据 ["上海", "北京", "深圳", "广州", "重庆"] 每根折线图对应的name
+      const legendArr = valueArr.map(item => {
+        return item.name
+      })
       // 数据配置项
-      const dataOption = {}
+      const dataOption = {
+        xAxis: {
+          data: timeArr
+        },
+        legend: {
+          data: legendArr // 筛选数据 内部name必须与seriesArr中name对应
+        },
+        series: seriesArr
+      }
       // 渲染图表
       this.chartInstance.setOption(dataOption)
     },
